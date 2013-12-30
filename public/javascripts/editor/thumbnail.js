17dd0c831720b7e39f77fe7715a256ccd7bc5cb7
Chris Zieba <zieba.chris@gmail.com>
2013-12-30 12:26:48 -0800

    Update copyright year

--- public/javascripts/editor/thumbnail.js
+++ public/javascripts/editor/thumbnail.js
@@ -1,4 +1,4 @@
-/*  Copyright 2013 Chris Zieba <zieba.chris@gmail.com>
+/*  Copyright 2014 Chris Zieba <zieba.chris@gmail.com>
 
     This program is free software: you can redistribute it and/or modify it under the terms of the GNU
     Affero General Public License as published by the Free Software Foundation, either version 3 of the
