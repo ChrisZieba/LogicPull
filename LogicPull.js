c3a8ab04a37cea4f2a897a79020920115c6a1e60
Chris Zieba <zieba.chris@gmail.com>
2015-05-31 14:47:45 -0700

    Change versions

--- LogicPull.js
+++ LogicPull.js
@@ -1,4 +1,4 @@
-/*	Copyright 2013 Chris Zieba
+/*	Copyright 2015 Chris Zieba
 
 	This program is free software: you can redistribute it and/or modify it under the terms of the GNU
 	Affero General Public License as published by the Free Software Foundation, either version 3 of the
