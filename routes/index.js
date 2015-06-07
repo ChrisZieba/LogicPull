ce74618e168bd3d1403088725f894ffd0c1ad044
Chris Zieba <zieba.chris@gmail.com>
2015-06-06 23:27:38 -0700

    Update spacing

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,45 @@
-/*	Copyright 2014 Chris Zieba <zieba.chris@gmail.com>
+/*  Copyright 2014 Chris Zieba <zieba.chris@gmail.com>
 
-	This program is free software: you can redistribute it and/or modify it under the terms of the GNU
-	Affero General Public License as published by the Free Software Foundation, either version 3 of the
-	License, or (at your option) any later version.
-	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-	without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-	PURPOSE. See the GNU Affero General Public License for more details. You should have received a
-	copy of the GNU Affero General Public License along with this program. If not, see
-	<http://www.gnu.org/licenses/>.
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+  Affero General Public License as published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE. See the GNU Affero General Public License for more details. You should have received a
+  copy of the GNU Affero General Public License along with this program. If not, see
+  <http://www.gnu.org/licenses/>.
 */
 
 var mongoose = require('mongoose'),
-	bcrypt = require('bcrypt'),
-	auth = require('../middleware/interviews/auth'),
-	fs = require('fs'),
-	nodemailer = require("nodemailer"),
-	models = require('../models/models'),
-	sanitizor = require('../lib/validation/sanitizor'),
-	validator = require('../lib/validation/validator');
+  bcrypt = require('bcrypt'),
+  auth = require('../middleware/interviews/auth'),
+  fs = require('fs'),
+  nodemailer = require("nodemailer"),
+  models = require('../models/models'),
+  sanitizor = require('../lib/validation/sanitizor'),
+  validator = require('../lib/validation/validator');
 
 
 module.exports = function (app) {
-	"use strict";
+  "use strict";
 
-	app.get('/', function (req, res) {
-		var interviews = models.Interviews.find({});
+  app.get('/', function (req, res) {
+    var interviews = models.Interviews.find({});
 
-		// make sure the group id of the logged in user matches that from the URL
-		interviews = interviews.where('disabled').equals(false).where('live').equals(true);
-		interviews.exec(function(err, interviews) {
-			if (err) {
-				console.log(err);
-				throw err;
-			}
+    // make sure the group id of the logged in user matches that from the URL
+    interviews = interviews.where('disabled').equals(false).where('live').equals(true);
+    interviews.exec(function(err, interviews) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
 
-			// send the output to the view
-			res.render('index', { 
-				title: 'LogicPull',
-				layout: 'interviews',
-				interviews: interviews,
-			});
-		});
-	});
+      // send the output to the view
+      res.render('index', { 
+        title: 'LogicPull',
+        layout: 'interviews',
+        interviews: interviews,
+      });
+    });
+  });
 };
--- routes/interviews/index.js
+++ routes/interviews/index.js
@@ -1,42 +1,42 @@
-/*	Copyright 2014 Chris Zieba <zieba.chris@gmail.com>
+/*  Copyright 2014 Chris Zieba <zieba.chris@gmail.com>
 
-	This program is free software: you can redistribute it and/or modify it under the terms of the GNU
-	Affero General Public License as published by the Free Software Foundation, either version 3 of the
-	License, or (at your option) any later version.
-	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-	without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-	PURPOSE. See the GNU Affero General Public License for more details. You should have received a
-	copy of the GNU Affero General Public License along with this program. If not, see
-	<http://www.gnu.org/licenses/>.
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+  Affero General Public License as published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE. See the GNU Affero General Public License for more details. You should have received a
+  copy of the GNU Affero General Public License along with this program. If not, see
+  <http://www.gnu.org/licenses/>.
 */
 
 var mongoose = require('mongoose'),
-	bcrypt = require('bcrypt'),
-	auth = require('../../middleware/interviews/auth'),
-	fs = require('fs'),
-	nodemailer = require("nodemailer"),
-	models = require('../../models/models'),
-	sanitizor = require('../../lib/validation/sanitizor'),
-	validator = require('../../lib/validation/validator');
+  bcrypt = require('bcrypt'),
+  auth = require('../../middleware/interviews/auth'),
+  fs = require('fs'),
+  nodemailer = require("nodemailer"),
+  models = require('../../models/models'),
+  sanitizor = require('../../lib/validation/sanitizor'),
+  validator = require('../../lib/validation/validator');
 
 module.exports = function (app) {
-	"use strict";
+  "use strict";
 
-	// This is the live interview available to anyone
-	app.get('/interviews/active/:interview', [auth.validateInterview], function (req, res) {
-		var interview = res.locals.interview;
+  // This is the live interview available to anyone
+  app.get('/interviews/active/:interview', [auth.validateInterview], function (req, res) {
+    var interview = res.locals.interview;
 
-		// this checks to see if the interview is live
-		if (interview.live) {
-			res.render('interviews/viewer', {
-				title: 'LogicPull - Interview ' + interview.name,
-				id: interview.id,
-				description: interview.description,
-				name: interview.name,
-				env: app.settings.env
-			});
-		} else {
-			res.status(404).render('interviews/404', {name: ''});	
-		}
-	});
+    // this checks to see if the interview is live
+    if (interview.live) {
+      res.render('interviews/viewer', {
+        title: 'LogicPull - Interview ' + interview.name,
+        id: interview.id,
+        description: interview.description,
+        name: interview.name,
+        env: app.settings.env
+      });
+    } else {
+      res.status(404).render('interviews/404', {name: ''}); 
+    }
+  });
 };
