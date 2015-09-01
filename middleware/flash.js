a086bad045f818eaf7b2b270eebd95d505cff272
Chris Zieba <zieba.chris@gmail.com>
2015-08-31 19:28:21 -0700

    Formatting

--- middleware/flash.js
+++ middleware/flash.js
@@ -81,12 +81,13 @@ function _flash (type, msg) {
  */
 module.exports = function flash (options) {
   options = options || {};
-  var safe = (options.unsafe === undefined) ? true : ! options.unsafe;
+  var safe = (options.unsafe === undefined) ? true : !options.unsafe;
   
   return function (req, res, next) {
     if (req.flash && safe) { 
       return next(); 
     }
+    
     req.flash = _flash;
     next();
   };
