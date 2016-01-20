e2c3876cbb3deb2365631f3124de8abb52182466
Chris Zieba <zieba.chris@gmail.com>
2016-01-19 22:46:52 -0800

    Update init script

--- bin/db/init.js
+++ bin/db/init.js
@@ -10,68 +10,68 @@
     <http://www.gnu.org/licenses/>.
 */
 
-db.inactives.remove();
-db.tmps.remove();
-db.states.remove();
-db.saves.remove();
-db.users.remove();
-db.groups.remove();
-db.interviews.remove();
-db.outputs.remove();
-db.counters.remove();
-db.sessions.remove();
+db.inactives.remove({});
+db.tmps.remove({});
+db.states.remove({});
+db.saves.remove({});
+db.users.remove({});
+db.groups.remove({});
+db.interviews.remove({});
+db.outputs.remove({});
+db.counters.remove({});
+db.sessions.remove({});
 db.dropDatabase();
 
 db.groups.insert({
-    "id" : 1,
-    "name": "Admin",
-    "description": "Admin"
+  "id" : 1,
+  "name": "Admin",
+  "description": "Admin"
 });
 
 db.users.insert({
-    "id" : 1,
-    "name": "Admin",
-    // The username is admin
-    "email": "admin@fake.com",
-    // The password is admin
-    "password": "$2a$10$6r6e74CXgfkiCSvX5NbG3eCPt3Eyw39pgh21K/.QvP9EQJxOWBWq6",
-    "group": 1,
-    "privledges": {
-        "add_interview": true,
-        "clone_interview": true,
-        "remove_interview": true,
-        "edit_interviews": true,
-        "change_interview_status": true,
-        "view_users": true,
-        "edit_user": true,
-        "add_user": true,
-        "reset_user_password": true,
-        "remove_user": true,
-        "add_deliverable": true,
-        "remove_deliverable": true,
-        "update_deliverable": true,
-        "lock_interview": true,
-        "edit_privledges": true,
-        "download_deliverable": true,
-        "download_stylesheet": true,
-        "download_answer_set": true,
-        "view_report": true,
-        "view_completed_interviews": true,
-        "view_saved_interviews": true,
-        "edit_on_complete": true,
-        "editor_save": true
+  "id" : 1,
+  "name": "Admin",
+  // The username is admin
+  "email": "admin@fake.com",
+  // The password is admin
+  "password": "$2a$10$6r6e74CXgfkiCSvX5NbG3eCPt3Eyw39pgh21K/.QvP9EQJxOWBWq6",
+  "group": 1,
+  "privledges": {
+    "add_interview": true,
+    "clone_interview": true,
+    "remove_interview": true,
+    "edit_interviews": true,
+    "change_interview_status": true,
+    "view_users": true,
+    "edit_user": true,
+    "add_user": true,
+    "reset_user_password": true,
+    "remove_user": true,
+    "add_deliverable": true,
+    "remove_deliverable": true,
+    "update_deliverable": true,
+    "lock_interview": true,
+    "edit_privledges": true,
+    "download_deliverable": true,
+    "download_stylesheet": true,
+    "download_answer_set": true,
+    "view_report": true,
+    "view_completed_interviews": true,
+    "view_saved_interviews": true,
+    "edit_on_complete": true,
+    "editor_save": true
     },
-    "created": new ISODate("2013-12-01T22:00:00.000Z")
+  "created": new ISODate("2013-12-01T22:00:00.000Z")
 });
 
 // Keep track of counts
 db.counters.insert({
-    "inactives": 0,
-    "tmp_count":0,
-    "state_count": 0,
-    "saved_count":0,
-    "user_count": 1,
-    "group_count": 1,
-    "interview_count": 0, 
-    "output_count": 0
+  "inactives": 0,
+  "tmp_count":0,
+  "state_count": 0,
+  "saved_count":0,
+  "user_count": 1,
+  "group_count": 1,
+  "interview_count": 0, 
+  "output_count": 0
 });
