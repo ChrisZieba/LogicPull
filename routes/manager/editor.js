621155a101649bf61332a0e34a3d842656d7f003
Chris Zieba <zieba.chris@gmail.com>
2018-03-11 05:48:52 -0700

    Update editor routes

--- routes/manager/editor.js
+++ routes/manager/editor.js
@@ -11,6 +11,7 @@
 */
 
 var bcrypt = require('bcryptjs'),
+  graphviz = require('graphviz'),
   fs = require('fs'),
   sanitizor = require('../../lib/validation/sanitizor'),
   validator = require('../../lib/validation/validator'),
@@ -67,8 +68,7 @@ module.exports = function (app) {
 
     if (!doc) {
       return res.json({
-        name: 'srv_error', 
-        error: interview.error("The interview could not be found.").error.content, 
+        name: 'srv_error',
         valid: false
       });
     }
@@ -86,19 +86,56 @@ module.exports = function (app) {
     });
   });
 
-  app.post('/manager/interview/:interview/edit/graph', [auth.validated, auth.validateInterview, auth.validateUserGroup, auth.privledges('edit_interviews')], function (req, res) {
+  app.post('/manager/interview/:interview/edit/order_graph', [
+    auth.validated, 
+    auth.validateInterview, 
+    auth.validateUserGroup, 
+    auth.privledges('editor_save')], function (req, res) {
+    var doc = res.locals.interview;
+    var data = req.body;
 
-  });
+    if (app.get('disable_graphviz_tidy')) {
+      return res.json({});
+    }
 
-  app.post('/manager/interview/:interview/edit/graph_reorder', [auth.validated, auth.validateInterview, auth.validateUserGroup, auth.privledges('edit_interviews')], function (req, res) {
+    var g = graphviz.digraph("G");
+    var options = {
+      type: "dot",
+      G: {
+        splines: false,
+        rankdir: "BT",
+        nodesep: "0.2"
+      }
+    };
 
-  });
+    // this creates the initial dot file to be rendered
+    for (var prop in data.nodes) {
+      if (data.nodes.hasOwnProperty(prop)) {
+        g.addNode(prop);
+        if (data.nodes[prop]) {
+          for (var i = 0; i < data.nodes[prop].length; i+=1) {
+            g.addEdge(prop, data.nodes[prop][i]);
+          }                   
+        }                   
+      }
+    }
 
-  app.post('/manager/interview/:interview/edit/get_saved_note', [auth.validated, auth.validateInterview, auth.validateUserGroup, auth.privledges('edit_interviews')], function (req, res) {
+    // this takes the dote graph generated above and creates a dot file with all the postions
+    g.output(options, function (out) { 
+      var dot = out.toString('utf-8');
+      var regex = /(q\d+)\s\[pos="(\d+),(\d+)",/gmi;
+      var graph = {};
+      var match;
 
-  });
+      while ((match = regex.exec(dot)) !== null) {
+        graph[match[1].toString()] = {
+          x: parseInt(match[2],10),
+          y: parseInt(match[3],10)
+        };
+      }
 
-  app.post('/manager/interview/:interview/edit/get_saved_note', [auth.validated, auth.validateInterview, auth.validateUserGroup, auth.privledges('edit_interviews')], function (req, res) {
+      return res.json({ graph: graph });
+    });
 
   });
 };
